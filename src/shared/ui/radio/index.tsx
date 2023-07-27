7f1cb4b5d2115dc6411886cdd49f40b53527e85d
Nikita Naumov <naumov99.rst@gmail.com>
2023-07-27 20:37:45 +0300

    chore: delete log

--- src/shared/ui/radio/index.tsx
+++ src/shared/ui/radio/index.tsx
@@ -2,8 +2,6 @@ import s from "./styles.module.css";
 type RadioProps = { value: string };
 
 export const Radio = (props: RadioProps) => {
-	console.log(props);
-
 	return (
 		<label className={s.Root}>
 			<input type="radio" className={s.Input} {...props} />
